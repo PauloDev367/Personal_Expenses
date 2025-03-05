e2dd2ff41eee1b0b56c0eb8856d4726a39b92289
Paulo Silva <pauloecomercial367@gmail.com>
2025-03-04 21:01:56 -0300

    :sparkles: project models

--- personalexpenses/account/models.py
+++ personalexpenses/account/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Base(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    class Meta:
+        abstract = True
+
+class Account(Base):
+    name = models.CharField(max_length=255)
+    balance = models.FloatField()
+    user_id = models.ForeignKey(User, related_name='account_user_id', on_delete=models.SET_NULL, null=True)
--- personalexpenses/accounttransaction/models.py
+++ personalexpenses/accounttransaction/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+from account.models import Account
 
-# Create your models here.
+class Base(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    class Meta:
+        abstract = True
+
+class AccountTransactionType(models.TextChoices):
+    EXPENSE = 'expense', 'Expense'
+    INCOME = 'income', 'Income'
+
+class AccountTransaction(Base):
+    type_choices = ('expense', 'income')
+    user_id = models.ForeignKey(User, related_name='account_transactions_user_id', on_delete=models.SET_NULL, null=True)
+    account_id = models.ForeignKey(Account, related_name='account_transactions_account_id', on_delete=models.SET_NULL, null=True)
+    amount = models.FloatField()
+    account_type = models.CharField(
+        max_length=30,
+        choices=AccountTransactionType.choices
+    )
+    category = models.CharField(max_length=150)
+    date = models.DateTimeField()
+    description = models.TextField()
