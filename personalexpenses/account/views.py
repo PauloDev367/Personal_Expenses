4c153834a02dcf2ca9438f8e9b2d7cc016947527
Paulo Silva <pauloecomercial367@gmail.com>
2025-03-05 08:28:42 -0300

    :sparkles: solicitando token de autenticacao

--- personalexpenses/account/views.py
+++ personalexpenses/account/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render
 from rest_framework import viewsets
 from .serializers import AccountSerializer
 from .models import Account
+from rest_framework.permissions import IsAuthenticated
 
 class AccountViewSet(viewsets.ModelViewSet):
     queryset = Account.objects.all()
     serializer_class = AccountSerializer
+    permission_classes = [IsAuthenticated]
     
     def get_queryset(self):
         return Account.objects.filter(user=self.request.user)
--- personalexpenses/accounttransaction/views.py
+++ personalexpenses/accounttransaction/views.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render
 from rest_framework import viewsets
 from .serializers import AccountTransactionSerializer
 from .models import AccountTransaction
+from rest_framework.permissions import IsAuthenticated
 
 class AccountTransactionViewSet(viewsets.ModelViewSet):
     queryset = AccountTransaction.objects.all()
     serializer_class = AccountTransactionSerializer
+    permission_classes = [IsAuthenticated]
     
     def get_queryset(self):
         return AccountTransaction.objects.filter(user=self.request.user)
